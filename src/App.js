5229ce9f60faafc8ccdc8049bc720c96c7c23ce9
Isabel Caballero <isabelcaballeromotta@hotmail.com>
2024-02-20 16:29:53 -0500

    props aprendidas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -5,22 +6,33 @@ import { TodoItem } from './TodoItem';
 import './App.css';
 import { CreaterTodoButton } from './CreateTodoButton';
 
+
+const defaultTodos= [
+  {text: 'Lavar loza', completed: false},
+  {text: 'Barrer', completed: false},
+  {text: 'Hacer almuerzo', completed: true},
+  {text: 'Sacar a Lupe 3 veces', completed: false}
+];
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter />
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreaterTodoButton />
-      
-    </div>
+    </React.Fragment>
+
   );
 }
 
